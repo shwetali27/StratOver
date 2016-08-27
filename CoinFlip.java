271a1c3e138e891be6e27a6d40f1b069a8bdcad3
-g <khambeshwetali@ymail.com>
2016-08-27 18:54:31 +0530

    info is added

--- CoinFlip.java
+++ CoinFlip.java
@@ -1,13 +1,19 @@
-//2. Program for flipping the coin and finding the percentage of Heads and Tails
+/*
+File Name: CoinFlip.java
+Created By: Shwetali
+Created Date: 27-08-2016
+purpose: Program for flipping the coin and finding the percentage of Heads and Tails
+*/
+
 package com.bridgelabz.programs;
 import com.bridgelabz.util.Utility;
 
 public class CoinFlip{
 	public static void main(String[] args){
-		double n,h=0,t=0,d;
+		double num,haid=0,tail=0;
 		Utility u = new Utility();
 		System.out.println("Please Enter the number of times you want to flip the coin");
-		n = u.inputDouble();
-		u.randFlip(n);
+		num = u.inputDouble();
+		u.randFlip(num);
 	}
 }
