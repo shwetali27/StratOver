16bfe2c972f98c2e42e8b2895a626e7fd99da6ef
shwetali27 <khambeshwetali@ymail.com>
2016-08-31 17:12:12 +0530

    Updated to search and file Operations

--- HashTableCreation.java
+++ HashTableCreation.java
@@ -8,8 +8,11 @@
 package com.bridgelabz.programs;
 import com.bridgelabz.util.Utility;
 import com.bridgelabz.programs.NodeCreation;
+import java.io.File;
 
 public class HashTableCreation{
+	Utility u = new Utility();
+	File file = new File("ListValues.txt");
 	NodeCreation head,temp,entry;
 	int key,value,tableSize;
 	NodeCreation[] table;
@@ -39,13 +42,85 @@ public class HashTableCreation{
 
 	}//end of addValue
 
+	//search value from table
+	public void search(int value){
+		int key = value % tableSize;
+		if(table[key] == null){
+			System.out.println("Search not Found, number is added");
+			this.addValue(value);
+			this.updateFile();
+		}
+		else{
+			head = table[key];
+			temp = head.getNext();
+			entry = new NodeCreation();
+			entry.setData(value);
+			int count = 0;
+			while(temp != null){
+				if(entry.getData() == temp.getData()){
+					System.out.println("Search Found, number is Deleted");
+					count++;
+					this.remove(value);
+					this.updateFile();
+					break;
+				}
+				else
+					temp = temp.getNext();
+			}
+			if(count == 0){
+				System.out.println("Search not found, value is added");
+				this.addValue(value);
+				this.updateFile();
+			}
+		}
+	}//end of search function
+
+	//removing value
+	public void remove(int value){
+		int key = value % tableSize;
+		head = table[key];
+		temp = head;
+		NodeCreation temp2 = temp.getNext();
+		while(temp2 != null){
+			if(value == temp2.getData()){
+				temp.setNext(temp2.getNext());
+				break;			
+			}
+			else{
+				temp = temp2;
+				temp2 = temp2.getNext();
+			}
+		}
+			
+	}
+
+	//Updating into file
+	public void updateFile(){
+		u.clearFile(file);
+		String str;
+		for(int i=0;i<tableSize;i++){
+			entry = table[i];
+			temp = table[i];
+			if(temp == null)
+				str = "";
+			else{
+				temp = temp.getNext();
+				while(temp != null){
+					str = Integer.toString(temp.getData());
+					u.writeFile(file,str);
+					temp = temp.getNext();
+				}
+			}
+		}
+	System.out.println("File is Updated");
+	}//end of updateFile
 		
 	//show table
 	public void showTable(){
 		for(int i=0;i<tableSize;i++){
 			entry = table[i];
 
-			System.out.print("\n"+i+": ");
+			System.out.print("\ntable "+i+": ");
 			temp = table[i];
 			int total = 0;
 			if(temp == null)
--- HashingFunction.java
+++ HashingFunction.java
@@ -8,6 +8,7 @@
 package com.bridgelabz.programs;
 import com.bridgelabz.util.Utility;
 import com.bridgelabz.programs.HashTableCreation;
+import java.io.File;
 
 public class HashingFunction{
 	public static void main(String[] args){
@@ -17,20 +18,27 @@ public class HashingFunction{
 		System.out.print("Please Enter Table Size:");
 		int size = u.inputInteger();
 		HashTableCreation ht = new HashTableCreation(size);
+		File file = new File("ListValues.txt");
+		String string = u.readFile(file);
+		String str[] = string.split(" ");
+		for(int i=0;i<str.length;i++){
+			ht.addValue(Integer.parseInt(str[i]));
+		}
+
 		while(check){
-			System.out.println("Please Choose Your Option:\n1.Enter Value\n2.Show HashTable\n3.Exit");
+			System.out.println("Please Choose Your Option:\n1.search Value\n2.Show HashTable\n3.Exit");
 			int choice = u.inputInteger();
 			switch(choice){
 				case 1:{
 					System.out.print("Please Enter a value: ");
 					int value = u.inputInteger();
-					ht.addValue(value);
-					System.out.println("value added");
+					ht.search(value);
 					break;
 				}
 				case 2:{
 					System.out.println("Hash table is");
 					ht.showTable();
+					System.out.println();
 					break;
 				}
 				case 3:{
