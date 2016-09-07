6f9d558b61d4c4e261aa845f77af62d03c743eb8
shwetali27 <khambeshwetali@ymail.com>
2016-09-07 16:43:18 +0530

    Some More options added

--- ClinicManagementProgram.java
+++ ClinicManagementProgram.java
@@ -11,43 +11,43 @@ import com.bridgelabz.programs.Doctor;
 import com.bridgelabz.programs.Patient;
 import com.bridgelabz.programs.UnOrderedNodeCreation;
 import com.bridgelabz.programs.ArrangeAppointment;
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class ClinicManagementProgram{
 	public static void main(String[] args){
 		Utility u = new Utility();
-		ClinicManagementProgram clinic = new ClinicManagementProgram();
+		int patientId = 1;
 		ArrangeAppointment appointment = new ArrangeAppointment();
 
-		LinkedList<Doctor> doctors = new LinkedList();
-		LinkedList<Patient> patients = new LinkedList();
-		//UnOrderedNodeCreation DoctNode = new UnOrderedNodeCreation();
+		ArrayList<Doctor> doctors = new ArrayList();
+		ArrayList<Patient> patients = new ArrayList();
 
+		//Creating doctors
 		Doctor doctor1 = new Doctor("Dr. Joshi",1,"Heart","Morning");
-		//DoctNode.setData(doctor1);
-		Doctor doctor2 = new Doctor("Dr. Ptel",2,"Eyes","Morning");
+		Doctor doctor2 = new Doctor("Dr. Patel",2,"Eyes","Morning");
 		Doctor doctor3 = new Doctor("Dr. Naved",3,"Brain","Evening");
 		Doctor doctor4 = new Doctor("Dr. Desai",4,"General","Morning/Evening");
 		Doctor doctor5 = new Doctor("Dr. Jeet",5,"Skin","Evening");
 
+		//Adding Doctors in ArrayList
 		doctors.add(doctor1);
 		doctors.add(doctor2);
 		doctors.add(doctor3);
 		doctors.add(doctor4);
 		doctors.add(doctor5);
-		//Doctor d = (Doctor)DoctNode.getData();
-		//new Doctor().printDoctor(d);
-		Patient patient1 = new Patient("Mr. Patil",1,"9087645342",30);
+
+		//Adding patients and taking appointments
+		Patient patient1 = new Patient("Mr. Patil",patientId,"9087645342",30);
 		patients.add(patient1);
-		//appointment.takeAppoinment(patient1,"Dr. Joshi",1);
-
-		Patient patient2 = new Patient("Ms. Sandhya",2,"8456985426",20);
+		appointment.takeAppoinment(patient1,1);
+		patientId++;
+		Patient patient2 = new Patient("Ms. Sandhya",patientId,"8456985426",20);
 		patients.add(patient2);
-		//appointment.takeAppoinment(patient1,"Dr. Patel",2);
-
-		Patient patient3 = new Patient("Mrs. Yadav",3,"7545865412",33);
+		appointment.takeAppoinment(patient2,1);
+		patientId++;
+		Patient patient3 = new Patient("Mrs. Yadav",patientId,"7545865412",33);
 		patients.add(patient3);
-		//appointment.takeAppoinment(patient1,"Dr. Jeet",5);
+		appointment.takeAppoinment(patient3,5);
 
 
 		//Display Options to perform operations.
@@ -55,7 +55,7 @@ public class ClinicManagementProgram{
 		while(check){
 			System.out.println("\n\nWelcome, Please choose Your Option");
 			System.out.println("1.Doctors List\n2.Patients List\n3.Search Doctor\n4.Search Patient");
-			System.out.println("5.Take Appoinment\n6.Popular Doctor \n7.Exit");
+			System.out.println("5.Take Appoinment\n6.Show All Appointments\n7.Popular Doctor \n8.Exit");
 			int choice = u.inputInteger();			
 			switch(choice){
 				case 1:{
@@ -80,23 +80,34 @@ public class ClinicManagementProgram{
 					break;
 				}
 				case 5:{
+					//taking patient's info and appointment
+					patientId++;
+					boolean checkId = true;
 					System.out.print("Please Enter Patient's Name: ");
 					String name = u.inputString();
-					System.out.print("Please Enter ID: ");
-					int id = u.inputInteger();
 					System.out.print("Please Enter Phone number: ");
 					String number = u.inputString();
 					System.out.print("Please Enter Age: ");
 					int age = u.inputInteger();
-					Patient newPatient = new Patient(name,id,number,age);
+
+					Patient newPatient = new Patient(name,patientId,number,age);
 					patients.add(newPatient);
+
+					System.out.print("Please enter Doctors id: ");
+					int docId = u.inputInteger();
+					appointment.takeAppoinment(newPatient,docId);
 					break;
 				}
 				case 6:{
-					new Doctor().popularDoctor();
+					appointment.showAppointments();
 					break;
 				}
+		
 				case 7:{
+					new Doctor().popularDoctor(doctors);
+					break;
+				}
+				case 8:{
 					check = false;
 					break;
 				}
@@ -105,7 +116,7 @@ public class ClinicManagementProgram{
 					break;
 				}
 			}//end of switch
-		}//end pf while
+		}//end of while
 		
 	}
 }
