5c8290ccacb3535f21482c663f39984306badd43
shwetali27 <khambeshwetali@ymail.com>
2016-09-09 10:12:59 +0530

    Test Program

--- JSONReadWrite.java
+++ JSONReadWrite.java
@@ -6,7 +6,6 @@
 */
 package com.bridgelabz.json;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -18,21 +17,21 @@ import com.bridgelabz.json.Utility;
 
 public class JSONReadWrite {
 	JSONParser parser = new JSONParser();
-	
+	int id;
 	Utility u = new Utility();
 	File file = new File("PatientsList.json");
 	
 	JSONObject patients = new JSONObject();
 	JSONArray patientsArray = new JSONArray();
 	
-	public void writeIntoFile(String name,int id,String number,int age){
+	public void writeIntoFile(String name,String number,int age){
 		JSONObject patient = new JSONObject();
 		patientsArray.add(patient);
 		patient.put("Name",name);
 		patient.put("ID",new Integer(id));
-		patient.put("MobileNo.",number);
+		patient.put("MobileNo",number);
 		patient.put("Age",new Integer(age));
-		
+		id++;
 		patients.put("Patients",patientsArray);
 		
 		try {
@@ -47,7 +46,7 @@ public class JSONReadWrite {
 	}
 
 	//reading file using parsing method
-	public void readInfoFile(){
+	public void readFile(){
 		try{
 			Object object = parser.parse(new FileReader(file));
 			JSONObject Patients = (JSONObject) object;//json object created
@@ -57,21 +56,34 @@ public class JSONReadWrite {
 				JSONObject patient = (JSONObject) patientsArray.get(y);
 				Object obj = patient.get("Name");
 				String name = (String)obj;
-				System.out.println("Name is:"+name);
-				
 				obj = patient.get("ID");
-				int id = Integer.parseInt(String.valueOf(obj));
-				System.out.println("ID is:"+id);
-				
+				int idInfo = Integer.parseInt(String.valueOf(obj));
+				this.id = idInfo;
 				obj = patient.get("MobileNo");
-				System.out.print("Checking no obj: "+obj);
 				String number = (String)obj;
-				System.out.println("Number is:"+number);
 				
 				obj = patient.get("Age");
 				int age = Integer.parseInt(String.valueOf(obj));
-				System.out.println("Age is:"+age);
-				this.writeIntoFile(name,id,number,age);
+				this.writeIntoFile(name,number,age);
+			}
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+	}
+	
+	public void readInfoFile(){
+		try{
+			Object object = parser.parse(new FileReader(file));
+			JSONObject Patients = (JSONObject) object;//json object created
+			
+			JSONArray patientsArray = (JSONArray) Patients.get("Patients");
+			for (int y = 0; y < patientsArray.size(); y++) {
+				JSONObject patient = (JSONObject)patientsArray.get(y) ;
+				System.out.println("Name is: "+patient.get("Name"));
+				System.out.println("ID is: "+patient.get("ID"));
+				System.out.println("Number is: "+patient.get("MobileNo"));
+				System.out.println("Age is: "+patient.get("Age"));
 				System.out.println();
 			}
 		}
