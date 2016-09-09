53771b6182439d003d7b96cea9a97e7b76fa5fc5
shwetali27 <khambeshwetali@ymail.com>
2016-09-09 10:13:21 +0530

    Test Program

--- JSONWriteReadMain.java
+++ JSONWriteReadMain.java
@@ -13,27 +13,30 @@ public class JSONWriteReadMain {
 	public static void main(String[] args) {
 		Utility u = new Utility();
 		JSONReadWrite fileObject = new JSONReadWrite();
-		int id = 0;
+		int time = 0;
 		boolean check = true;
 		while(check){
 			System.out.println("Please Select Option:\n1.Write\n2.Read\n3.Exit");
 			int choice = u.inputInteger();
 			switch(choice){
 				case 1:{
-					id++;
 					
-					System.out.println("Please Enter Name");
+					if(time==0)
+						fileObject.readFile();
+					time++;
+					System.out.print("Please Enter Name: ");
 					String name = u.inputString();
-					System.out.println("Please Enter Number");
+					System.out.print("Please Enter Number: ");
 					String number = u.inputString();
-					System.out.println("Please Enter Age");
+					System.out.print("Please Enter Age: ");
 					int age = u.inputInteger();
 					
-					fileObject.writeIntoFile(name,id,number,age);
+					fileObject.writeIntoFile(name,number,age);
 					break;
 				}
 				case 2:{
 					fileObject.readInfoFile();
+					break;
 				}
 			}
 		}//end while
